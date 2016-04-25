b922cb00c53690b838782395c10e87559bfb487a
Thijs Zumbrink <thijs@targetmedia.nl>
2016-04-25 20:23:16 +0200

    Do some general setup

--- tests/codeception/functional/HomeCept.php
+++ tests/codeception/functional/HomeCept.php
@@ -6,6 +6,3 @@ $I = new FunctionalTester($scenario);
 $I->wantTo('ensure that home page works');
 $I->amOnPage(Yii::$app->homeUrl);
 $I->see('My Company');
-$I->seeLink('About');
-$I->click('About');
-$I->see('This is the About page.');
